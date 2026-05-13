dbb84fa426923185c7014a7877ab093a36154d18
sledzienamasle <piotras0303@gmail.com>
2026-05-13 19:56:54 +0200

    nowy tarjan

--- Tarjan.py
+++ Tarjan.py
@@ -1,69 +1,72 @@
-from DFS import dfs
-
-
 class Tarjan:
-    """Algorytm Tarjana do znajdowania silnie spójnych składowych (SCC)"""
-    
-    def __init__(self, graph):
-        """
-        Inicjalizacja algorytmu Tarjana
-        
-        Args:
-            graph: słownik reprezentujący graf {wierzchołek: [lista sąsiadów]}
-        """
-        self.graph = graph
-        self.index_counter = 0
+    def __init__(self, lista_nastepnikow):
+        self.graph = lista_nastepnikow
+
+        self.index = 0
+
         self.stack = []
-        self.indices = {}
-        self.lowlinks = {}
-        self.on_stack = {}
+        self.on_stack = set()
+
+        self.indices = [-1] * len(lista_nastepnikow)
+        self.lowlink = [-1] * len(lista_nastepnikow)
+
         self.sccs = []
-    
-    def stronglyConnectedComponents(self):
-        """
-        Znajduje wszystkie silnie spójne składowe w grafie
-        
-        Returns:
-            Lista list reprezentujących silnie spójne składowe
-        """
-        for v in self.graph:
-            if v not in self.indices:
-                self._tarjan_dfs(v)
-        
-        return self.sccs
-    
-    def _tarjan_dfs(self, v):
-        """
-        Głębokie przeszukiwanie z algorytmu Tarjana
-        
-        Args:
-            v: bieżący wierzchołek
-        """
-        self.indices[v] = self.index_counter
-        self.lowlinks[v] = self.index_counter
-        self.index_counter += 1
+
+    def dfs(self, v):
+
+        # ustawienie indeksu
+        self.indices[v] = self.index
+        self.lowlink[v] = self.index
+
+        self.index += 1
+
         self.stack.append(v)
-        self.on_stack[v] = True
-        
-        # Przejdź wszystkie krawędzie v -> w
-        if v in self.graph:
-            for w in self.graph[v]:
-                if w not in self.indices:
-                    # Wierzchołek nie był odwiedzony
-                    self._tarjan_dfs(w)
-                    self.lowlinks[v] = min(self.lowlinks[v], self.lowlinks[w])
-                elif self.on_stack.get(w, False):
-                    # Krawędź zwrotna do wierzchołka na stosie
-                    self.lowlinks[v] = min(self.lowlinks[v], self.indices[w])
-        
-        # Jeśli v jest wierzchołkiem głównym, wypakuj stos i utwórz SCC
-        if self.lowlinks[v] == self.indices[v]:
+        self.on_stack.add(v)
+
+        # przechodzenie po sasiadach
+        for neighbor in self.graph[v]:
+
+            # jesli nieodwiedzony
+            if self.indices[neighbor] == -1:
+
+                self.dfs(neighbor)
+
+                self.lowlink[v] = min(
+                    self.lowlink[v],
+                    self.lowlink[neighbor]
+                )
+
+            # jesli jest na stosie
+            elif neighbor in self.on_stack:
+
+                self.lowlink[v] = min(
+                    self.lowlink[v],
+                    self.indices[neighbor]
+                )
+
+        # znaleziono SCC
+        if self.lowlink[v] == self.indices[v]:
+
             scc = []
+
             while True:
+
                 w = self.stack.pop()
-                self.on_stack[w] = False
+
+                self.on_stack.remove(w)
+
                 scc.append(w)
+
                 if w == v:
                     break
-            
+
             self.sccs.append(scc)
+
+    def find_scc(self):
+
+        for v in range(len(self.graph)):
+
+            if self.indices[v] == -1:
+                self.dfs(v)
+
+        return self.sccs
